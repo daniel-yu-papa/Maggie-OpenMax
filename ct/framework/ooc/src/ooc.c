14d2912b3f7f8ef16f50e4c4bee6353634cc4c21
yujun <yujun@marvell.com>
2013-09-04 14:21:56 +0800

    add the ooc module

---
+++ ct/framework/ooc/src/ooc.c
@@ -0,0 +1,258 @@
+#include "ooc.h"
+#include <string.h>
+#include <stdlib.h>
+#include "Mag_base.h"
+
+/**	Base Class.
+ * Used for root Class in ooc. It must be a superclass for all classes.
+ */
+
+ROM_ALLOC struct ClassTable BaseClass;
+
+
+/* Class initialization
+ */
+
+static
+void
+invalid_virtual_function( void )
+{
+    MAG_ASSERT(1);
+}
+
+
+/*	Calculating the offset of the first virtual function in the Vtable.
+	Must be done this tricky, because the following assumption is not necessarily true:
+	sizeof(struct BaseVtable) == offsetof( _dummyVtable, first_virtual_function)
+	This assumption may fail, depending on the compiler and the system ! */
+
+struct _dummyVtable {
+	struct	BaseVtable_stru	Base;
+	void	(* first_virtual_function)( void );
+};
+#define virtual_function_alignment (offsetof( struct _dummyVtable, first_virtual_function))
+
+static
+void
+invalidate_vtable( const Class type )
+{
+	size_t i;
+	void (**fnp)(void);
+
+	if( type->vtab_size > virtual_function_alignment ) {
+    	i = ( type->vtab_size - virtual_function_alignment ) / sizeof( void(*)() );
+    	fnp = (void (**)(void)) ( ((char*) type->c.vtable) + virtual_function_alignment);
+
+    	for( ; i ; i--,  fnp++ )
+    		*fnp = invalid_virtual_function;
+    }
+}
+
+static
+void
+inherit_vtable_from_parent( const Class self )
+{
+	if( ooc_class_has_parent(self) ) {
+
+		MAG_ASSERT( self->vtab_size >= self->parent->vtab_size );
+		
+		/* Inherit the overridden operators */
+		self->c.vtable->_destroy_check = self->parent->c.vtable->_destroy_check;
+
+		/* Inherit the virtual functions */
+		if( self->parent->vtab_size > virtual_function_alignment )
+		    memcpy( ((char *) self->c.vtable)+ virtual_function_alignment,			/* destination */
+			        ((char *) self->parent->c.vtable) + virtual_function_alignment,	/* source */
+			        self->parent->vtab_size - virtual_function_alignment );		    /* bytes to copy */
+		}
+}
+
+
+void
+_ooc_init_class( const Class self )
+{
+	if( self->c.vtable->_class == NULL ) {
+
+		if( ooc_class_has_parent( self ) )
+			_ooc_init_class( self->parent );
+
+		self->c.vtable->_class = self;
+		self->c.vtable->_destroy_check = NULL;
+
+		invalidate_vtable( self );
+
+		inherit_vtable_from_parent( self );
+
+		self->init( self );
+	}
+}
+
+int
+_ooc_isInitialized( const Class type )
+{
+	return ( type == & BaseClass || type->c.vtable->_class == type ) ? TRUE : FALSE;
+}
+
+void *
+ooc_calloc( size_t num, size_t size )
+{
+	void * allocated;
+
+	allocated = calloc( num, size );
+
+	return allocated;
+}
+
+static __inline__
+void
+ooc_build_object( Object object, const Class type, const void * params )
+{
+	/* Building the Object header */
+	object->_vtab = type->c.vtable;		/* Vtable pointer */
+	
+	MAG_ASSERT( sizeof( struct BaseObject ) == sizeof( struct BaseVtable * ) );
+	/* If struct BaseObject has been changed, additional initialization might be missing here! */
+
+	/* Constructs the object instance, that may fail */
+	type->ctor( object, params );	
+}
+
+
+Object
+ooc_new_classptr( const Class type, const void * params )
+{
+	Object object;
+
+	/* The class type must be initialized already! */
+	MAG_ASSERT( _ooc_isInitialized( type ) );
+
+	/* Allocates a memory block for the object instance, and initializes it with zeros */
+	object = ooc_calloc(  1, type->size );
+
+    if (NULL != object)
+	{
+    	ooc_build_object( object, type, params );
+
+    	return object;
+	}else{
+        return NULL;
+    }
+}
+
+/************************************
+* Delete the Object
+************************************/
+/*  Helper: pointer read-out while nulling
+ */
+
+void *
+ooc_ptr_read_and_null( void ** ptr_ptr )
+{
+    OOC_IMPLEMENT_PTR_READ_AND_NULL
+}
+
+void
+ooc_free( void * mem )
+{
+	if( mem )
+		free( mem );
+}
+
+static
+void
+ooc_destroy_object( Object self )
+{
+	Class type, next;
+	Vtable vtab;
+
+	MAG_ASSERT( self != NULL );
+	
+	/* Makes the Object invalid */
+	vtab = ooc_ptr_read_and_null( (void**) &self->_vtab );
+	
+	if( vtab )
+	{
+		next = vtab->_class;
+		
+		do {
+			type = next;
+
+			type->dtor( self, vtab );
+	
+			next = type->parent;
+		} while( ooc_class_has_parent( type ) );
+	}
+}
+
+void
+ooc_delete( Object self )
+{
+	if( self && self->_vtab )
+		if( self->_vtab->_destroy_check == NULL || (self->_vtab->_destroy_check)(self) == TRUE )
+		{
+			ooc_destroy_object( self );
+			ooc_free( self );
+		}
+}
+
+void
+ooc_delete_and_null( Object * obj_ptr )
+{
+	MAG_ASSERT( obj_ptr );
+
+	/* Thread safe release of the object */
+	ooc_delete( (Object) ooc_ptr_read_and_null( (void**) obj_ptr ) );
+}
+
+/************************************
+* Safty Check
+************************************/
+static
+int
+ooc_isClassChildOf( const Class checkable, const Class base )
+{
+    Class actual = checkable;
+    
+    do {
+        if( actual->parent == base )
+            return TRUE;
+        actual = actual->parent;
+        } while( actual );
+
+    return FALSE;
+}
+
+
+int
+_ooc_isInstanceOf( const Object self, const Class base )
+{
+	MAG_ASSERT( _ooc_isInitialized( base ) );
+
+	if( ! self )
+		return FALSE;
+
+	if( ! self->_vtab )
+		return FALSE;
+
+	if( ! self->_vtab->_class )
+		return FALSE;
+
+	if( self->_vtab->_class == base )
+		return TRUE;
+
+	if( self->_vtab->_class->c.vtable != self->_vtab )
+		return FALSE;
+
+	return ooc_isClassChildOf( self->_vtab->_class, base );
+}
+
+void
+ooc_check_cast( void * _self, const Class target )
+{
+	if( ! _ooc_isInstanceOf( _self, target ) )
+		MAG_ASSERT(1);
+		
+	return;
+}
+
+
---
+++ ct/framework/ooc/test/ooc_test.c
@@ -0,0 +1,162 @@
+#include "ooc_test.h"
+#include "agilelog.h"
+#include <stdlib.h>
+#include <string.h>
+#include "Mag_base.h"
+
+/*Parent class: Polygon*/
+AllocateClass(Polygon, Base);
+
+int virtual_polygon_get_edges(Polygon thiz){
+    AGILE_LOGD("[%s]: edges number is %d", __FUNCTION__, thiz->mEdgeNum);
+
+    return 0;
+}
+
+int Polygon_setColor(Polygon thiz, int colour){
+    thiz->mColor = colour;
+    AGILE_LOGD("[%s]: set color %d", __FUNCTION__, colour);
+    return 0;
+}
+
+int Polygon_getColor(Polygon thiz){
+    AGILE_LOGD("[%s]: get color %d", __FUNCTION__, thiz->mColor);
+    return thiz->mColor;
+}
+
+void Polygon_setOwner(Polygon thiz, char *owner){
+    strcpy(thiz->mpOwner, owner);
+    AGILE_LOGD("[%s]: set owner %s", __FUNCTION__, owner);
+}
+
+char *Polygon_getOwner(Polygon thiz){
+    AGILE_LOGD("[%s]: get owner %s", __FUNCTION__, thiz->mpOwner);
+    return thiz->mpOwner;
+}
+
+static void Polygon_initialize(Class this){
+    PolygonVtableInstance.getEdges = virtual_polygon_get_edges;
+}
+
+static void Polygon_constructor(Polygon thiz, const void *params){
+    MAG_ASSERT(ooc_isInitialized(Polygon));
+    chain_constructor(Polygon, thiz, params);
+    thiz->setColor = Polygon_setColor;
+    thiz->getColor = Polygon_getColor;
+    thiz->setOwner = Polygon_setOwner;
+    thiz->getOwner = Polygon_getOwner;
+
+    thiz->mpOwner = (char *)malloc(16);
+    thiz->mEdgeNum = *((int *)params + 0);
+    thiz->mColor = *((int *)params + 1);
+    AGILE_LOGD("[%s]: EdgeNum = %d, Color = %d", __FUNCTION__, thiz->mEdgeNum, thiz->mColor);
+}
+
+static void Polygon_destructor(Polygon thiz, PolygonVtable vtab){
+    free(thiz->mpOwner);
+    AGILE_LOGD("[%s]", __FUNCTION__);
+}
+
+/*Child class: Triangle*/
+AllocateClass(Triangle, Polygon);
+
+int virtual_triangle_sellToCustomer(Triangle thiz, int money){   
+    thiz->mTotalPrice = thiz->mBasePrice + money;
+    AGILE_LOGD("[%s]: total price = %d, base price = %d, money = %d", __FUNCTION__, 
+                thiz->mTotalPrice, thiz->mBasePrice, money);
+}
+
+
+int virtual_triangle_get_edges(Polygon thiz){
+    Triangle self = ooc_cast(thiz, Triangle);
+    AGILE_LOGD("[%s]: edges number is %d", __FUNCTION__, self->mEdgeNum);
+
+    return 0;
+}
+
+int Triangle_bidBasePrice(Triangle thiz, int price){
+    AGILE_LOGD("[%s]: bidding price is %d", __FUNCTION__, price);
+    thiz->mBasePrice = price;
+    return 0;
+}
+
+static void Triangle_initialize(Class this){
+    TriangleVtableInstance.sellToCustomer = virtual_triangle_sellToCustomer;
+    /*override the parent's virtual function*/
+    TriangleVtableInstance.Polygon.getEdges = virtual_triangle_get_edges;
+}
+
+static void Triangle_constructor(Triangle thiz, const void *params){
+    MAG_ASSERT(ooc_isInitialized(Triangle));
+    chain_constructor(Triangle, thiz, params);
+
+    thiz->bidBasePrice = Triangle_bidBasePrice;
+
+    AGILE_LOGD("[%s]", __FUNCTION__);
+
+    thiz->mEdgeNum = *((int *)params + 0);
+}
+
+static void Triangle_destructor(Triangle thiz, TriangleVtable vtab){
+    AGILE_LOGD("[%s]", __FUNCTION__);
+}
+
+/*Child class: TriangleInGold*/
+AllocateClass(TriangleInGold, Triangle);
+
+int virtual_triangleInGold_sellToCustomer(Triangle thiz, int money){   
+    TriangleInGold self = ooc_cast(thiz, TriangleInGold);
+    self->mTotalPrice = self->mBasePrice + money * 10;
+    AGILE_LOGD("[%s]: total price = %d, base price = %d, money = %d", __FUNCTION__, 
+                self->mTotalPrice, self->mBasePrice, money);
+}
+
+static void TriangleInGold_initialize(Class this){
+    /*override the parent's virtual function*/
+    TriangleInGoldVtableInstance.Triangle.sellToCustomer = virtual_triangleInGold_sellToCustomer;
+}
+
+int TriangleInGold_bidBasePrice(TriangleInGold thiz, int price){
+    AGILE_LOGD("[%s]: bidding price is %d", __FUNCTION__, price);
+    thiz->mBasePrice = price;
+    return 0;
+}
+
+static void TriangleInGold_constructor(TriangleInGold thiz, const void *params){
+    MAG_ASSERT(ooc_isInitialized(TriangleInGold));
+    chain_constructor(TriangleInGold, thiz, params);
+
+    thiz->bidBasePrice = TriangleInGold_bidBasePrice;
+
+    AGILE_LOGD("[%s]", __FUNCTION__);
+}
+
+static void TriangleInGold_destructor(TriangleInGold thiz, TriangleInGoldVtable vtab){
+    AGILE_LOGD("[%s]", __FUNCTION__);
+}
+
+int main( int argc, char argv[] ){
+    TriangleInGold goldTri;
+    Triangle tri;
+    Polygon parent;
+    
+    int param[2] = {3, 1};
+    
+    ooc_init_class(TriangleInGold);
+
+    goldTri = (TriangleInGold) ooc_new( TriangleInGold, (void *)param);
+
+    goldTri->bidBasePrice(goldTri, 1888);
+    TriangleInGoldVirtual(goldTri)->Triangle.sellToCustomer(ooc_cast(goldTri, Triangle), 1000);
+
+    tri = ooc_cast(goldTri, Triangle);
+    TriangleVirtual(tri)->Polygon.getEdges(ooc_cast(tri, Polygon));
+    
+    parent = ooc_cast(goldTri, Polygon);
+    //parent->setColor(parent, 12);
+    parent->getColor(parent);
+    parent->setOwner(parent, "Yu Jun");
+   
+    ooc_delete((Object)goldTri);
+}
+
