720e80b156d7f235ae4012bc20d9008e42777de5
yujun <yujun@marvell.com>
2013-09-25 16:47:42 +0800

    add red-black tree

---
+++ ct/framework/base/inc/Mag_rbtree.h
@@ -0,0 +1,143 @@
+#ifndef __MAG_RBTREE_H__
+#define __MAG_RBTREE_H__
+
+#include <stdlib.h>
+#include "agilelog.h"
+
+typedef enum{
+    RBTREE_FALSE = 0,
+    RBTREE_TRUE  = !RBTREE_FALSE,
+}RBTREE_BOOL;
+
+typedef enum{
+    NODE_RED = 0,
+    NODE_BLACK = 1,
+}NODE_COLOR_t;
+
+typedef struct rbtree_node_t{
+    int          key;
+    void*        value;
+    NODE_COLOR_t color;
+    
+    struct rbtree_node_t *left;
+    struct rbtree_node_t *right;   
+}RBTREE_NODE_t;
+
+typedef RBTREE_NODE_t* RBTreeNodeHandle;
+
+static inline RBTREE_BOOL isRed(RBTreeNodeHandle x){
+    if (NULL == x) return RBTREE_FALSE;
+    if (x->color == NODE_RED) return RBTREE_TRUE;
+    return RBTREE_FALSE;
+}
+
+static inline void reverseColor(NODE_COLOR_t *color){
+    if (*color == NODE_RED)
+        *color = NODE_BLACK;
+    else
+        *color = NODE_RED;
+}
+
+static inline RBTreeNodeHandle rotateLeft(RBTreeNodeHandle h){
+    RBTreeNodeHandle x = h->right;
+
+    h->right = x->left;
+    x->left = h;
+
+    x->color = x->left->color;
+    x->left->color = NODE_RED;
+    return x;
+}
+
+static inline RBTreeNodeHandle rotateRight(RBTreeNodeHandle h){
+    RBTreeNodeHandle x = h->left;
+
+    h->left = x->right;
+    x->right = h;
+
+    x->color = x->right->color;
+    x->right->color = NODE_RED;
+    return x;
+}
+
+static inline void colorFlip(RBTreeNodeHandle h){   
+    if ((NULL == h->left) ||
+        (NULL == h->right)){
+        AGILE_LOGE("Should not be here!! left(0x%x) right(0x%x)", h->left, h->right);
+        return;
+    }
+    reverseColor(&h->color);
+    reverseColor(&h->left->color);
+    reverseColor(&h->right->color);
+}
+
+/*the basic operations for delete action*/
+static inline RBTreeNodeHandle fixup(RBTreeNodeHandle h){
+    if (NULL == h)
+        return NULL;
+    
+    /*rotate-left for red-leaning reds*/
+    if (isRed(h->right))
+        h = rotateLeft(h);
+
+    /*rotate right for red-red pairs*/
+    if (isRed(h->left) && (NULL != h->left) && isRed(h->left->left))
+        h = rotateRight(h);
+
+    /*split 4-nodes*/
+    if (isRed(h->left) && isRed(h->right))
+        colorFlip(h);
+
+    return h;
+}
+
+static inline RBTreeNodeHandle moveRedRight(RBTreeNodeHandle h){
+    colorFlip(h);   
+    if ((NULL != h->left) && isRed(h->left->left)){
+        h = rotateRight(h);
+        colorFlip(h);
+    }
+    return h;
+}
+
+static inline RBTreeNodeHandle moveRedLeft(RBTreeNodeHandle h){
+    colorFlip(h);
+    if ((NULL != h->right) && isRed(h->right->left)){
+        h->right = rotateRight(h->right);
+        h = rotateLeft(h);
+        colorFlip(h);
+    }
+    return h;
+}
+
+static inline RBTreeNodeHandle min(RBTreeNodeHandle h)
+{
+    if (h->left == NULL) 
+        return h;
+    else 
+        min(h->left);
+}
+
+
+static inline RBTreeNodeHandle deleteMin(RBTreeNodeHandle h)
+{ 
+    if ((h == NULL) || (h->left == NULL))
+     return NULL;
+
+    if (!isRed(h->left) && !isRed(h->left->left))
+     h = moveRedLeft(h);
+
+    h->left = deleteMin(h->left);
+
+    return fixup(h);
+}
+
+void *rbtree_get(RBTreeNodeHandle root, int key);
+RBTreeNodeHandle rbtree_insert(RBTreeNodeHandle root, int key, void *value);
+int rbtree_delete(RBTreeNodeHandle *root, int key);
+int rbtree_dump(RBTreeNodeHandle root, int print_flag);
+
+int rbtree_debug_getRepeatNum(void);
+
+
+#endif
---
+++ ct/framework/base/src/Mag_rbtree.c
@@ -0,0 +1,155 @@
+#include "Mag_rbtree.h"
+#include "Mag_mem.h"
+
+static int gLoopNum = 0;
+static int gRepeatKeyNum = 0;
+
+static RBTreeNodeHandle rbtree_get_priv(RBTreeNodeHandle root, int key){
+    RBTreeNodeHandle x = root;
+
+    while(NULL != x){
+        if (x->key == key) 
+            return x;
+        else if (x->key < key)
+            x = x->right;
+        else
+            x = x->left;
+    }
+    return NULL;
+}
+
+void *rbtree_get(RBTreeNodeHandle root, int key){
+    RBTreeNodeHandle x = root;
+
+    while(NULL != x){
+        if (x->key == key) 
+            return x->value;
+        else if (x->key < key)
+            x = x->right;
+        else
+            x = x->left;
+    }
+    return NULL;
+}
+
+RBTreeNodeHandle rbtree_insert(RBTreeNodeHandle root, int key, void *value){
+    /*insert the node at the bottom*/
+    if (NULL == root){
+        RBTreeNodeHandle node = (RBTreeNodeHandle)mag_malloc(sizeof(RBTREE_NODE_t));
+        if (NULL != node){
+            node->color = NODE_RED;
+            node->key   = key;
+            node->value = value;
+            node->left  = NULL;
+            node->right = NULL;
+        }
+        return node;
+    }
+
+    /*split 4-node on the way down*/
+    if (isRed(root->left) && isRed(root->right))
+        colorFlip(root);
+    
+    /*standard binary tree insert*/
+    if (root->key == key){
+        root->value = value;
+        ++gRepeatKeyNum;
+    }else if (root->key < key){
+        root->right = rbtree_insert(root->right, key, value);
+    }else{
+        root->left = rbtree_insert(root->left, key, value);
+    }
+    
+    /*fix right-leaning reds on the way up*/
+    if (isRed(root->right))
+        root = rotateLeft(root);
+
+    /*fix two reds in a row on the way up*/
+    if (isRed(root->left) && isRed(root->left->left))
+        root = rotateRight(root);
+    
+    return root;
+}
+
+static RBTreeNodeHandle rbtree_delete_priv(RBTreeNodeHandle root, int key, RBTreeNodeHandle *deleteNode){
+    if (NULL == root)
+        return NULL;
+    
+    if (key < root->key){
+        if (NULL == root->left)
+            return NULL;
+        
+        /*delete minimal node*/
+        if (!isRed(root->left) && !isRed(root->left->left))
+            root = moveRedLeft(root);
+        
+        root->left = rbtree_delete_priv(root->left, key, deleteNode);
+    }else{
+        if (isRed(root->left))
+            root = rotateRight(root);
+
+        /*delete leaf node*/
+        if (root->right == NULL){
+            if (root->key == key){
+                AGILE_LOGD("find the key: %d", key);
+                *deleteNode = root;
+                return NULL;
+            }
+        }
+
+        if (!isRed(root->right) && !isRed(root->right->left))
+            root = moveRedRight(root);
+        
+        if (root->key == key){
+            RBTreeNodeHandle min_node;
+            min_node = min(root->right);
+            AGILE_LOGD("the min key: %d", min_node->key);
+            root->key = min_node->key;
+            root->value = min_node->value;
+            root->right = deleteMin(root->right);
+            *deleteNode = min_node;
+        }else{
+            root->right = rbtree_delete_priv(root->right, key, deleteNode);
+        }
+    }
+
+    return fixup(root);
+}
+
+int rbtree_delete(RBTreeNodeHandle *root, int key){
+    RBTreeNodeHandle deleteNode;
+    if (rbtree_get_priv(*root, key) != NULL){
+        *root = rbtree_delete_priv(*root, key, &deleteNode);
+        if (NULL != deleteNode){
+            mag_free(deleteNode);
+        }
+        return 0;
+    }else{
+        return -1;
+    }
+}
+
+/*in-order traverse the binary tree*/
+static void rbtree_dump_priv(RBTreeNodeHandle root, int print_flag){
+    if (NULL == root)
+        return;
+
+    rbtree_dump_priv(root->left, print_flag);
+    if (print_flag)
+        AGILE_LOGI("node %d: [key:%d - value:%d - color:%s]", ++gLoopNum, root->key, *((int *)root->value), root->color == NODE_RED ? "RED" : "BLACK");
+    else
+        ++gLoopNum;
+    rbtree_dump_priv(root->right, print_flag);
+}
+
+int rbtree_dump(RBTreeNodeHandle root, int print_flag){
+    gLoopNum = 0;
+    rbtree_dump_priv(root, print_flag);
+    return gLoopNum;
+}
+
+int rbtree_debug_getRepeatNum(void){
+    return gRepeatKeyNum;
+}
+
+
