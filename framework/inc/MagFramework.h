3a2541deddb9baa756ae547e2efa40e456855bff
yujun <yujun@marvell.com>
2014-06-12 10:05:08 +0800

    complete the scons compiling system for framework module

--- framework/inc/MagFramework.h
+++ framework/inc/MagFramework.h
@@ -1,21 +1,22 @@
 #ifndef __MAGFRAMEWORK_H__
 #define __MAGFRAMEWORK_H__
 
-#include "agilelog_def.h"
-#include "pub/agilelog.h"
-#include "Mag_base.h"
+#include "Mag_pub_def.h"
+#include "Mag_pub_type.h"
+#include "Mag_agilelog.h"
+#include "Mag_agilelogInt.h"
+#include "Mag_hal.h"
 #include "Mag_list.h"
 #include "Mag_rbtree.h"
 #include "Mag_minidb.h"
 #include "Mag_event.h"
-#include "hashTable.h"
+#include "Mag_hashTable.h"
 #include "Mag_looper.h"
 #include "Mag_mem.h"
 #include "Mag_mempool.h"
 #include "Mag_msg.h"
 #include "Mag_msgQueue.h"
-#include "ooc.h"
+#include "Mag_ooc.h"
 #include "Mag_thread.h"
-#include "StreamBufferDef.h"
 
 #endif
