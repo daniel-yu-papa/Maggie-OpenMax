a281b4557557d26b677afa4d22e843370a9be1d0
yujun <yujun@marvell.com>
2014-09-24 17:41:21 +0800

    add linux-arm.mk

---
+++ linux-arm.mk
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [ $1 = "-c" ]; then
+	scons target=arm-unknown-linux build_config=Debug build_path_prefix=True -c
+else
+	scons target=arm-unknown-linux build_config=Debug build_path_prefix=True
+fi
+
+cp Targets/arm-unknown-linux/Debug/lib/libMagOmxIL_* ~/nfs/MagOmx_IL/
+cp Targets/arm-unknown-linux/Debug/lib/libMagOmxILTest_* ~/nfs/MagOmx_IL/comps/
+cp Targets/arm-unknown-linux/Debug/bin/MagOmxILTest_Playback ~/nfs/MagOmx_IL/
+cp Targets/arm-unknown-linux/Debug/lib/libMagFramework.so ~/nfs/MagOmx_IL/
+cp Targets/arm-unknown-linux/Debug/lib/libMagAgilelog.so ~/nfs/MagOmx_IL/
